2cb3ecffb5679f9b061adb3e7dcca7f1f138788b
Daniel Kelly <me@danielkelly.io>
2026-03-18 11:05:41 -0500

    chore: nuxt config format and runtime config fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+console.log("api key", process.env.NUXT_GOOGLE_GENERATIVE_AI_API_KEY);
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/mdc'],
+  modules: ["@nuxtjs/mdc"],
   runtimeConfig: {
-    googleGenerativeAiApiKey: process.env.GOOGLE_GENERATIVE_AI_API_KEY
-  }
-})
+    googleGenerativeAiApiKey: "",
+  },
+});
