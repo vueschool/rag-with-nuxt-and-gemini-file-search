4cf2d4e8cd93efdea2f9f3c539d7158d2ed2024b
Daniel Kelly <me@danielkelly.io>
2026-03-18 11:09:11 -0500

    chore: remove console log

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-console.log("api key", process.env.NUXT_GOOGLE_GENERATIVE_AI_API_KEY);
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
